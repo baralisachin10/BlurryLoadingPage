3605121ece3ddd24322eb351b072cad1784a676c
sachin <baralisachin441@gmail.com>
2022-12-10 00:29:56 +0545

    blurry loading page

---
+++ script.js
@@ -0,0 +1,34 @@
+const loadText = document.getElementById('loading-text');
+const bg = document.querySelector('.bg');
+
+
+
+let load = 0;
+
+// loadText.innerText = `${load}%`
+
+let int = setInterval(blurring , 30);
+
+function blurring() {
+    load++;
+        if(load > 99){
+             clearInterval(int);
+        }
+        loadText.innerText = `${load}%`;
+
+        loadText.style.opacity = scale(load , 0, 100, 1, 0);
+
+        bg.style.filter = `blur(${scale(load, 0, 100, 30 , 0)}px)`;
+        
+}
+
+
+// from stackOverflow for maping the range of value to another range of value
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+}
+
+
+
+
+
